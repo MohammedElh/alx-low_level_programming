dc9c1a38cc847c99de4818de443a03f3d50b123b
MohammedElh <Habachi20000@gmail.com>
2023-03-23 17:58:58 -0700

    hhh

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_line - entry point
+ *
+ * @n: number of times _ should be printed
+ *
+ * Return: nothing
+ */
+void print_line(int n)
+{
+	int underscore = 95;
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar(underscore);
+		}
+		_putchar('\n');
+	}
+}
